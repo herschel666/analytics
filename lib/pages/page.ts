2faaf50e5ac470824f33df6aaadd2550bf5a172b
Emanuel Kluge <mail@emanuel-kluge.de>
2020-10-03 08:33:37 +0200

    chore: remove unused inlining of CSS

--- lib/pages/page.ts
+++ lib/pages/page.ts
@@ -1,14 +1,3 @@
-import { join } from 'path';
-import { readFileSync as readFile } from 'fs';
-
-const insertCSS = (): string => {
-  try {
-    return `<style>${readFile(join(__dirname, 'index.css'), 'utf8')}</style>`;
-  } catch {
-    return '';
-  }
-};
-
 export const page = (body: string, title = 'ek|analytics'): string =>
   /* html */ `
 <!doctype html>
@@ -18,7 +7,6 @@ export const page = (body: string, title = 'ek|analytics'): string =>
     <title>${title}</title>
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link href="/_static/main.css" rel="stylesheet">
-    ${insertCSS()}
   </head>
   <body>
     ${body}
