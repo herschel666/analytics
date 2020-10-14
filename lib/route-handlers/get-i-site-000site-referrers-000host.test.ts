88c8991eec5e0debc665cf0f1024a470847deb82
Emanuel Kluge <mail@emanuel-kluge.de>
2020-10-14 08:12:06 +0200

    refactor: dumbify page function for device statistics by host

--- lib/route-handlers/get-i-site-000site-referrers-000host.test.ts
+++ lib/route-handlers/get-i-site-000site-referrers-000host.test.ts
@@ -1,17 +1,30 @@
+import type { Data } from '@architect/functions';
+
+import { getReferrersBySiteAndHost } from '../shared/ddb';
+import { siteNameToHostname } from '../shared/util';
 import { pageSiteReferrersHost } from '../pages/page-i-site-referrers-host';
 import { handler } from './get-i-site-000site-referrers-000host';
+
+jest.mock('../shared/ddb', () => ({
+  getReferrersBySiteAndHost: jest
+    .fn()
+    .mockResolvedValue([{ type: 'referrers-by-host' }]),
+}));
 
 jest.mock('../pages/page-i-site-referrers-host', () => ({
   pageSiteReferrersHost: jest.fn().mockReturnValue('some HTML...'),
 }));
 
 describe('get-i-site-000site-referrers-000host', () => {
+  const data = ({ analytics: 'analytics' } as unknown) as Data;
+  const referrers = [{ type: 'referrers-by-host' }];
   const site = 'site_tld';
   const host = 'some-other-site_com';
   const owner = 'some-user';
 
   it('should return successfully', async () => {
     const { statusCode, headers, body } = await handler({
+      data,
       owner,
       site,
       host,
@@ -23,17 +36,34 @@ describe('get-i-site-000site-referrers-000host', () => {
     expect(body).toBe('some HTML...');
   });
 
-  it('should call the view function', async () => {
+  it('should request data from the DDB', async () => {
     await handler({
+      data,
       owner,
       site,
       host,
     });
 
-    expect(pageSiteReferrersHost).toHaveBeenCalledWith(
+    expect(getReferrersBySiteAndHost).toHaveBeenCalledWith(
+      data.analytics,
       site,
       owner,
-      'some-other-site.com'
+      siteNameToHostname(host)
     );
+  });
+
+  it('should call the view function', async () => {
+    await handler({
+      data,
+      owner,
+      site,
+      host,
+    });
+
+    expect(pageSiteReferrersHost).toHaveBeenCalledWith({
+      referrerHostname: siteNameToHostname(host),
+      referrers,
+      site,
+    });
   });
 });
