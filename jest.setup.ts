eb2ddf451a0210d59f4507060f0a9646a130920a
Emanuel Kluge <mail@emanuel-kluge.de>
2020-10-26 21:49:36 +0100

    test: check OAuth flow

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,5 @@
 process.env.SECRET_KEY = 'secret-key';
 process.env.NODE_ENV = 'testing';
+process.env.HOSTNAME = 'app.url';
+process.env.GH_APP_CLIENT_ID = 'gh-client-id';
+process.env.GH_APP_CLIENT_SECRET = 'gh-client-secret';
