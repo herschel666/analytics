4afaa96299643754de0fcdbbc4066e3894588a32
Emanuel Kluge <mail@emanuel-kluge.de>
2020-11-09 20:48:46 +0100

    fix: render site selector for logged-in users only

--- lib/components/main-header.tsx
+++ lib/components/main-header.tsx
@@ -1,6 +1,7 @@
 import h from 'vhtml';
 import type { HC } from 'vhtml';
 
+import { Fragment } from './fragment';
 import { siteNameToHostname } from '../shared/util';
 
 export interface Props {
@@ -38,39 +39,41 @@ export const MainHeader: HC<Props> = ({ loggedIn, sites = [], text }) => {
         <NavbarBrand class="navbar-brand" {...navbarBrandProps}>
           {text || 'ek|analytics'}
         </NavbarBrand>
-        <nav
-          class={sitesSelectorClasses.join(' ')}
-          id="sites-selector"
-          data-turbolinks-permanent={true}
-        >
-          <div class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              id="navbarDropdownMenuLink"
-              role="button"
-              data-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Choose a site…
-            </a>
-            <div
-              class="dropdown-menu js-dropdown-menu"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              {sites.map((site) => (
-                <span>
-                  <a class="dropdown-item" href={`/i/site/${site}`}>
-                    {siteNameToHostname(site)}
-                  </a>
-                </span>
-              ))}
-            </div>
-          </div>
-        </nav>
         {loggedIn && (
-          <form action="/logout" method="post">
-            <button class="btn btn-outline-dark btn-sm">Logout</button>
-          </form>
+          <Fragment>
+            <nav
+              class={sitesSelectorClasses.join(' ')}
+              id="sites-selector"
+              data-turbolinks-permanent={true}
+            >
+              <div class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Choose a site…
+                </a>
+                <div
+                  class="dropdown-menu js-dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  {sites.map((site) => (
+                    <span>
+                      <a class="dropdown-item" href={`/i/site/${site}`}>
+                        {siteNameToHostname(site)}
+                      </a>
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </nav>
+            <form action="/logout" method="post">
+              <button class="btn btn-outline-dark btn-sm">Logout</button>
+            </form>
+          </Fragment>
         )}
       </div>
     </header>
